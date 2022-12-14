0317cdf86806a465fd3563f7c8036d832821825a
Timothy Chang <tchang135@gmail.com>
2022-12-14 14:43:34 -0500

    finished first routes phase 1

--- practice-for-ch-rails-first-routes-long-practice-main/firstroutes/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/firstroutes/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
+  # get 'users/', to: 'users#index', as: 'users'
+  # post 'users/', to: 'users#create'
+  # get 'users/new', to: 'users#new', as: 'new_user'
+  # get 'users/:id/edit', to: 'users#edit', as:'edit_user'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update' 
+  # delete 'users/:id', to: 'users#destroy'
+
 end
