a825e1f1cc2d18bfe827f34e035e5b7a10a2fa36
Timothy Chang <tchang135@gmail.com>
2022-12-14 17:25:30 -0500

    finished for day;

--- practice-for-ch-rails-first-routes-long-practice-main/firstroutes/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/firstroutes/app/controllers/users_controller.rb
@@ -18,22 +18,23 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
-        render json: @user
+        user = User.find(params[:id])
+        render json: user
     end
 
     def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            redirect_to user_url(@user)
+        debugger
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_url(user)
         else
-            render json: @user.errors.full_messages, status: :unprocessable_entity
+            render json: user.errors.full_messages, status: :unprocessable_entity
         end
     end
 
     def destroy
-        @user = User.find(params[:id])
-        @user.destroy
+        user = User.find(params[:id])
+        user.destroy
         redirect_to user_url
     end
 
