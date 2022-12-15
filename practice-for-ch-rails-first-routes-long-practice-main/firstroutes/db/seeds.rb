322e10e5b2b03edb12f199063a2bf9e016c16a69
Stiven Kang <kdaegyung@gmail.com>
2022-12-15 15:02:10 -0500

    finished phase 1

--- practice-for-ch-rails-first-routes-long-practice-main/firstroutes/db/seeds.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/firstroutes/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.create(username: "Stiven")
+# User.create(username: "Tim")
+# User.create(username: "Brandon")
+# User.create(username: "Dan")
+# User.create(username: "Shwilly")
+
+Artwork.create(title: "Dog Photo", image_url: "dog.pic", artist_id: User.first.id)
+Artwork.create(title: "Cat Photo", image_url: "cat.pic", artist_id: User.second.id)
+Artwork.create(title: "Sandwich Photo", image_url: "sandwichdrawing.pic", artist_id: User.last.id)
+
+ArtworkShare.create(artwork_id: Artwork.first.id, viewer_id: User.first.id)
+ArtworkShare.create(artwork_id: Artwork.second.id, viewer_id: User.second.id)
+ArtworkShare.create(artwork_id: Artwork.last.id, viewer_id: User.last.id)
